c0fa53123aeb142623dec2a77700b878d17794d4
Yaroslav <garasym@gmail.com>
2025-12-09 18:20:41 +0200

    selenium example

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,11 +1,46 @@
 package com.softserve.edu;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 /**
  * Hello world!
  */
 public class App {
+    private static final Long IMPLICITLY_WAIT_SECONDS = 10L;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello World!");
+        //
+        // Manual download
+        //System.setProperty("webdriver.chrome.driver", "./lib/chromedriver142.exe");
+        //
+        // Atomated
+        WebDriverManager.chromedriver().setup();
+        //
+        WebDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver.manage().window().maximize();
+        //
+        //driver.get("https://google.com.ua/");
+        driver.get("https://www.bing.com/");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.name("q")).sendKeys("mac");
+        Thread.sleep(2000); // For Presentation
+        //
+        //driver.findElement(By.cssSelector("button[type='submit']")).click();
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        //
+        Thread.sleep(8000); // For Presentation
+        driver.quit();
+        //
+        System.out.println("done");
     }
 }
---
+++ src/test/java/com/softserve/edu2sel/FirstTest.java
@@ -0,0 +1,63 @@
+package com.softserve.edu2sel;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FirstTest {
+    private final Long IMPLICITLY_WAIT_SECONDS = 10L;
+    private WebDriver driver;
+
+    @BeforeAll
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver.manage().window().maximize();
+    }
+
+    @AfterAll
+    public void tear() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void checkMac() throws InterruptedException {
+        driver.get("https://www.bing.com/");
+        Thread.sleep(1000); // For Presentation
+        //
+        driver.findElement(By.name("q")).sendKeys("mac");
+        Thread.sleep(1000); // For Presentation
+        //
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        Thread.sleep(1000); // For Presentation
+        //
+        driver.findElement(By.xpath("//a[text()='Mac - Apple']")).click();
+        Thread.sleep(1000); // For Presentation
+        //
+        // switch to another window
+        String originalWindow = driver.getWindowHandle();
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!originalWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        Thread.sleep(1000); // For Presentation
+        //
+        String apple = driver.getCurrentUrl();
+        Thread.sleep(1000); // For Presentation
+        //
+        Assertions.assertEquals("https://www.apple.com/mac/", apple);
+    }
+
+}
